dd163fb4aeec5a6f1aa58dc47499d06508de3a89
Monzer Abdullaziz <nezonru87@yahoo.com>
2022-02-18 10:01:46 +0200

    documents formatted

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
-/**
- * @license
- * Copyright 2019 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
 
-import {LitElement, html, css} from 'lit';
+import {
+  LitElement,
+  html,
+  css
+} from 'lit';
 
-/**
- * An example element.
- *
- * @fires count-changed - Indicates when the count changes
- * @slot - This element has a slot
- * @csspart button - The button
- */
 export class MyElement extends LitElement {
-  static properties (){
+  static properties() {
     this.name = {};
   }
 
@@ -24,7 +16,7 @@ export class MyElement extends LitElement {
   }
 
   render() {
-    return html`
+    return html `
       <h1>Hi ${this.name} saying your name from Lit</h1>
     `;
   }
